30eb60f7f3ee94406e76048885e6c13c4e16a5b2
JoChangMin20 <mine2060@naver.com>
2025-07-25 10:33:03 +0900

    최초 커밋

---
+++ Calculator.java
@@ -0,0 +1,9 @@
+// 작업1
+public class Calculator {
+    public static void main(String[] args) {
+        System.out.println("1 + 1 = 2");
+        System.out.println("1 - 1 = 0");
+        System.out.println("2 * 1 = 2");
+        System.out.println("2 / 1 = 2");
+    }
+}
